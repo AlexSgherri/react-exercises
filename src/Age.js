e3030658c84fa55270f190b752d04ffe091da3d8
Alessandro <a.sgherri92@gmail.com>
2022-08-25 10:18:09 +0200

    Conditiona_Rendering-02-done

--- src/Age.js
+++ src/Age.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export class Age extends React.Component{
+
     render(){
-        return this.props.age > 18 && <p>Your age is {this.props.age}</p>
+        return this.props.age >= 0 && <p>Your age is {this.props.age}</p>
     }
 }
