1c0d7379dba3aa7c4ea11b938bdc89258ed046dd
Alessandro <a.sgherri92@gmail.com>
2022-08-25 10:23:25 +0200

    Conditional_Rendering-03-done

--- src/Welcome.js
+++ src/Welcome.js
@@ -10,12 +10,8 @@ export class Welcome extends React.Component{
         return (
             <div>
                 <p>Welcome, {this.props.name}</p>
-                <Age age />  
+                <Age age={19} />  
             </div>
         )
     }
 }
-
-//la consegna dell'esercizio chiede solo l'esistenza, perciò se si inserisce solo age senza specificarne il contenuto o anche una stringa
-//verrà stampato solo il messaggio senza una specifica età o la stringa
-//la risoluzione sarebbe quella di mettere ad esempio un semplice - this.props.age >= 0 -
