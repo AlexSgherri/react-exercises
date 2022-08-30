b78c9485a87ef1203fd16d4b9c4d9fc2cc78d335
Alessandro <a.sgherri92@gmail.com>
2022-08-30 17:48:16 +0200

    removed the name===jhon condition

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Age } from "./Age";
 
 export class Welcome extends React.Component{
-    static defaultProps = {
-        name : "Alessandro",
-    }
+
     render(){
         return (
             <div>
                 <p>Welcome, {this.props.name}</p>
-                { this.props.name === "John" && <Age age={17} /> }
+                <Age age={19} />
             </div>
         )
     }
+}
+
+Welcome.defaultProps = {
+    name : "Alessandro",
 }
