226a0456ae4e46a05e739c869248865d399adb38
Alessandro <a.sgherri92@gmail.com>
2022-09-02 18:00:02 +0200

    useEffect-02-done

--- src/Counter.js
+++ src/Counter.js
@@ -1,32 +1,52 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export class Counter extends React.Component {
-  constructor(props) {
-    super(props);
+export function Counter({
+  initialValue = 0,
+  incrementInterval = 1000,
+  incrementAmount = 1,
+}) {
+  const [counter, setCounter] = useState(initialValue);
 
-    this.state = {
-      count: this.props.initialValue,
-    };
+  function incrementFunction() {
+    console.log("I have mounted")
+    setInterval(() => setCounter((c) => c + incrementAmount), incrementInterval);
+    return () => {
+      console.log("Unmounting");
+    }
   }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState((state) => {
-        return {
-          count: state.count + this.props.incrementAmount,
-        };
-      });
-    }, this.props.incrementInterval);
-  }
+  useEffect(incrementFunction, [incrementAmount, incrementInterval] );
 
-  render() {
-    return <CounterDisplay stateAsAProp={this.state.count} />;
-  }
+  return <CounterDisplay stateAsAProp={counter} />;
 }
 
-Counter.defaultProps = {
-  initialValue: 0,
-  incrementInterval: 1000,
-  incrementAmount: 1,
-};
+// export class Counter extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       count: this.props.initialValue,
+//     };
+//   }
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       this.setState((state) => {
+//         return {
+//           count: state.count + this.props.incrementAmount,
+//         };
+//       });
+//     }, this.props.incrementInterval);
+//   }
+
+//   render() {
+//     return <CounterDisplay stateAsAProp={this.state.count} />;
+//   }
+// }
+
+// Counter.defaultProps = {
+//   initialValue: 0,
+//   incrementInterval: 1000,
+//   incrementAmount: 1,
+// };
