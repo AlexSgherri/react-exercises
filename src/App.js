efa797d10ef63030f5bb05721e8887316c145e73
Alessandro <a.sgherri92@gmail.com>
2022-09-05 21:15:02 +0200

    useContext-done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ClickCounter } from "./ClickCounter";
 import { ClickTracker } from "./ClickTracker";
 import { Counter } from "./Counter";
@@ -16,51 +16,52 @@ import { GithubUser } from "./GithubUser";
 import { GithubUserList } from "./GithubUserList";
 import { CarDetails } from "./CarDetails";
 
-export class App extends React.Component{
-    constructor(props){
-        super(props);
+export function App (){
+    const [language, setLanguage] = useState("en");
 
-        this.state = {
-            language: 'en',
-            counterVisible: true
-        }
-    }
+  function languageChangeHandler(event) {
+   setLanguage(event.target.value);
+  };
 
-    languageChangeHandler = (event) =>{
-        this.setState({
-            language: event.target.value
-        })
-    }
+  function counterFunction (number) {
+    console.log(`The number is : ${number}`);
+  };
 
-    counterFunction = (number) => {
-        console.log(`The number is : ${number}`)
-    }
+ function onLogin(state) {
+    console.log(state);
+  }
 
-    onLogin(state){
-        console.log(state)
-    }
+//   function toggleCounter (){
+//     setState({ counterVisible: !state.counterVisible });
+//   };
 
-    toggleCounter = () => {
-        this.setState({counterVisible: !this.state.counterVisible})
-    }
-
-    render(){
-        return (
-        <Container title="React Exercises">
-            <Hello /> 
-            <Welcome name="John" />
+ 
+    return (
+      <Container title="React Exercises">
+        <Hello />
+        <Welcome name="John" />
+        <select
+          value={language}
+          onChange={languageChangeHandler}
+        >
+          <option value="en">English</option>
+          <option value="it">Italiano</option>
+        </select>
+        <LanguageContext.Provider value={language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
+        {/* 
             <CarDetails initialData={{model : "Renault", year : 1998, color: "#000000"}}/>
-            {/* 
             <GithubUser username="AlexSgherri" />
-            <Login passingFunction={this.onLogin} />
-            <ClickCounter onCounterChange={this.counterFunction}/>
+            <Login passingFunction={onLogin} />
+            <ClickCounter onCounterChange={counterFunction}/>
             <GithubUserList users={['IvanFras98', 'AlexSgherri']} />
-            <button onClick={this.toggleCounter} >Toggle Counter</button>
-            {this.state.counterVisible && <Counter initialValue={2} incrementAmount={3} incrementInterval={500} />}
+            <button onClick={toggleCounter} >Toggle Counter</button>
+            {state.counterVisible && <Counter initialValue={2} incrementAmount={3} incrementInterval={500} />}
             <Sum numbers={ [5, 7, 9, 12] } />
             <ClickTracker />
             <InteractiveWelcome />
-            <UncontrolledLogin passingFunction={this.onLogin} />
+            <UncontrolledLogin passingFunction={onLogin} />
             <TodoList render={(liElement, deleteFunction)=> {
                 return (
                     <ul>
@@ -77,14 +78,8 @@ export class App extends React.Component{
             }} >
             </TodoList >
             <br></br>
-            <select value={this.state.language} onChange={this.languageChangeHandler}>
-                <option value="en">English</option>
-                <option value="it">Italiano</option>
-            </select>
-            <LanguageContext.Provider value={this.state.language}>
-                <DisplayLanguage />
-            </LanguageContext.Provider> */}
-        </Container>
-        )
-    }
+
+            */}
+      </Container>
+    );
 }
--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,29 +1,18 @@
-import React from "react";
+import { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
 
 const lang = {
-    en : {
-        CURRENT_LANG : 'English language'
-    },
-    
-    it : {
-        CURRENT_LANG : 'Lingua Italiana'
-    }
+  en: {
+    CURRENT_LANG : "English language",
+  },
 
-}
+  it: {
+    CURRENT_LANG : "Lingua Italiana",
+  },
+};
 
-export class DisplayLanguage extends React.Component{
+export function DisplayLanguage() {
+  const language = useContext(LanguageContext);
 
-
-    render(){
-        return (
-            <>
-                <LanguageContext.Consumer>
-                    {(language) => {
-                        return <h1>{lang[language].CURRENT_LANG} ( key: {language } )</h1>
-                    }}
-                </LanguageContext.Consumer>
-            </>
-        )
-    }
+  return <h3>{lang[language].CURRENT_LANG} ( key: {language} )</h3>
 }
