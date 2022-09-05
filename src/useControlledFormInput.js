d3552ebd676136f7a791fc502abac835db200c56
Alessandro <a.sgherri92@gmail.com>
2022-09-05 09:10:32 +0200

    Custom_Hooks-02-done

---
+++ src/useControlledFormInput.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function useControlledFormInput() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    remember: false,
+  });
+
+  const inputEventHandlers = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const type = event.target.type;
+    const checked = event.target.checked;
+
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const resetStateHandler = () => {
+    setFormData({
+      username: "",
+      password: "",
+      remember: false,
+    });
+  };
+
+  return {
+    formData: formData,
+    inputEventHandlers: inputEventHandlers,
+    resetStateHandler: resetStateHandler,
+  };
+}
