8afc02d2282808fc7950bb1b393a488b93b98497
Alessandro <a.sgherri92@gmail.com>
2022-09-07 14:53:57 +0200

    SWR-03-done

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -4,13 +4,14 @@ import { useParams } from "react-router-dom";
 
 export function GithubUser() {
   const { username } = useParams()
-  const {user, error, loading} = useGithubUser(username)
+  const {user, error, loading, refreshData} = useGithubUser(username)
   
   return (
     <div>
       {loading && <h1>Loading...</h1>}
       {user && (
         <>
+          <button onClick={refreshData}>Refresh</button>
           <h1>{user.name}</h1>
           <ul>
             <li>Login : {user.login}</li>
